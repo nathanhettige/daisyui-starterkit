6871df8e77bc82c984de32c0b61d7c8baa735b6e
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2022-11-12 16:27:35 +1000

    eslint failOnError

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from "vite-plugin-eslint";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
+    plugins: [react(), eslint({ failOnError: mode !== 'development' })]
+  };
 });
