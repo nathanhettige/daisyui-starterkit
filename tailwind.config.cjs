9ae2aa94a3195652824aa1f4d0cc1fd308c8fd01
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-01-09 15:48:55 +1000

    Add meta tags and links in index.html for PWA

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     themes: [
       {
         mytheme: {
+          // Remember to set the theme-colour meta tag in index.html
           primary: '#6419E6',
           secondary: '#D926A9',
           accent: '#1FB2A6',
