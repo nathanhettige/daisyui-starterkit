a6d6ee89bff52c03af7b015c24a852762fe9c992
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-01-12 11:22:46 +1000

    Move Avatars to  Avatars.tsx

---
+++ src/theme/components/Avatars.tsx
@@ -0,0 +1,26 @@
+export const Avatars = (
+  <div className="flex justify-center">
+    <div className="space-x-2">
+      <div className="avatar offline placeholder">
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-24">
+          <span className="text-3xl">K</span>
+        </div>
+      </div>
+      <div className="avatar online placeholder">
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-16">
+          <span className="text-xl">JO</span>
+        </div>
+      </div>
+      <div className="avatar placeholder">
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-12 ring ring-primary">
+          <span>MX</span>
+        </div>
+      </div>
+      <div className="avatar placeholder">
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-8">
+          <span className="text-xs">AA</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -3,3 +3,4 @@ export * from './Text';
 export * from './Inputs';
 export * from './Miscellanous';
 export * from './Alerts';
+export * from './Avatars';
