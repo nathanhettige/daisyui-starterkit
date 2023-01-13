41334d65eec9aa4a35d89062f85064f422db09f8
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-01-13 20:17:40 +1000

    Remove miscellanous components

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,7 +1,6 @@
 export * from './Buttons';
 export * from './Text';
 export * from './Inputs';
-export * from './Miscellanous';
 export * from './Alerts';
 export * from './Avatars';
 export * from './Badges';
