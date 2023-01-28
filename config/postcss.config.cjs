099555798a104077b5ce196dd9e0e0cf124fb145
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-01-28 16:42:39 +1000

    Move tailwind config

--- config/postcss.config.cjs
+++ config/postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: { config: './config/tailwind.config.cjs' },
     autoprefixer: {}
   }
 };
---
+++ config/tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#6419E6',
+          secondary: '#D926A9',
+          accent: '#1FB2A6',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272'
+        }
+      }
+    ]
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/container-queries')]
+};
