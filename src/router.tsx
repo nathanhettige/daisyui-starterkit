c33fc314ded0e0943c7a59ca766f92dd41abae9a
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-02-08 22:42:51 +1000

    Remove lazy load

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,9 @@
-import { PageLoading } from '@ui/utils/PageLoading';
-import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import ThemePage from './pages/ThemePage';
 
 /* Code split theme page */
-const ThemePage = lazy(async () => await import('./pages/ThemePage'));
+// const ThemePage = lazy(async () => await import('./pages/ThemePage'));
 
 const router = createBrowserRouter(
   [
@@ -15,9 +14,9 @@ const router = createBrowserRouter(
     {
       path: '/theme',
       element: (
-        <Suspense fallback={<PageLoading />}>
-          <ThemePage />
-        </Suspense>
+        // <Suspense fallback={<PageLoading />}>
+        <ThemePage />
+        // </Suspense>
       )
     }
   ],
