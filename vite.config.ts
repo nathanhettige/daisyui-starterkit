da19d429ab90ca1f19fc19e112f9eb7976d8e431
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-02-09 19:06:08 +1000

    Replace babel with SWC

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from 'tailwindcss';
