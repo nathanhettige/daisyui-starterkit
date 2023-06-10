445daa4d1f084dcf8d2c35a8d799dbd19631d656
Nathan Hettige <nathan.hettige@gmail.com>
2023-06-11 00:38:47 +1000

    Make router a FC

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './api/ReactQueryClient';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import router from './router';
+import Router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <RouterProvider router={router} />
+      <Router />
     </QueryClientProvider>
   </React.StrictMode>
 );
