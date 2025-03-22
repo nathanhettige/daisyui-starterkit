a25dcd994684cdadb80349a9118f15ab8d9e5ffa
Nathan Hettige <nathan.hettige@gmail.com>
2025-03-22 21:01:53 +1000

    feat: update react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './api/ReactQueryClient';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
